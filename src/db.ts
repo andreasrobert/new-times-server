bf11f0ebf4385f1d2c5d8013d91796b5013a38a2
andreasrobert <andreasrobb23@gmail.com>
2021-08-12 16:27:28 +0700

    try deploy2

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,30 @@
 import { Pool } from "pg";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // export const pool = new Pool({
-//     user: process.env.PG_USER,
-//     host: process.env.PG_HOST,
-//     password: process.env.PG_PASSWORD,
-//     database: process.env.PG_DATABASE,
+//     user: "postgres",
+//     host: "localhost",
+//     password: "one2three",
+//     database: "pernstack",
 //     port: 5432,
 //   });
+
+// export const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+//   port: Number(process.env.PG_DATABASE),
+// });
 
 const devConfig = {
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-  port: 5432,
+  port: Number(process.env.PG_DATABASE),
 };
 
 const proConfig = {
