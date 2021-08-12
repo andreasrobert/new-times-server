9aa238d25a68bf2e154e0d2116ff546744de6067
andreasrobert <andreasrobb23@gmail.com>
2021-08-12 17:22:03 +0700

    try7

--- src/db.ts
+++ src/db.ts
@@ -9,7 +9,8 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 const proConfig = process.env.DATABASE_URL;
 
 export const pool = new Pool ({
-  connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
+  // connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
+  connectionString: proConfig 
 })
 
 
