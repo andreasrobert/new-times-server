6395a98e3b93cbe4afb95e4117042c99999beb64
andreasrobert <andreasrobb23@gmail.com>
2021-08-12 17:02:34 +0700

    try deploy5

--- src/db.ts
+++ src/db.ts
@@ -3,21 +3,47 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const devConfig = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: Number(process.env.PG_DATABASE),
-};
 
-const proConfig = {
-  connectionString: process.env.DATABSE_URL,
-};
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-export const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+const proConfig = process.env.DATABASE_URL;
+
+export const pool = new Pool ({
+  connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+//   port: Number(process.env.PG_DATABASE),
+// };
+
+// const proConfig = {
+//   connectionString: process.env.DATABSE_URL,
+// };
+
+// export const pool = new Pool(
+//   process.env.NODE_ENV === "production" ? proConfig : devConfig
+// );
 
 
 // export const pool = new Pool({
