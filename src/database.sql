ac8615434e19e1dac3b002c68d6ed802bcc17ce0
andreasrobert <andreasrobb23@gmail.com>
2021-08-12 15:44:34 +0700

    try depolyment

--- src/database.sql
+++ src/database.sql
@@ -4,3 +4,31 @@ CREATE TABLE todo(
   todo_id SERIAL PRIMARY KEY,
   description VARCHAR(255)
 );
+
+CREATE TABLE user(
+  id SERIAL PRIMARY KEY,
+  username varchar(255) not null,
+  password VARCHAR(255) not null
+);
+
+CREATE TABLE posts(
+  id serial primary key,
+  title text not null,
+  body text default '...',
+  creator_id int references users(id) not null,
+  votes
+);
+
+CREATE TABLE comment(
+  id SERIAL PRIMARY KEY,
+  message text not null,
+  post_id int references posts(id),
+  creator_id int references user(id)
+);
+
+
+create table favorites(
+   user_id int references users(id),
+   post_id int references posts(id),
+   primary key (user_id, post_id) -- composite key
+);
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,25 @@
-import { Pool } from "pg"
+import { Pool } from "pg";
 
-export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'one2three',
-    database: 'pernstack',
-    port: 5432,
-  });
+// export const pool = new Pool({
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     password: process.env.PG_PASSWORD,
+//     database: process.env.PG_DATABASE,
+//     port: 5432,
+//   });
+
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  port: 5432,
+};
+
+const proConfig = {
+  connectionString: process.env.DATABSE_URL,
+};
+
+export const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
